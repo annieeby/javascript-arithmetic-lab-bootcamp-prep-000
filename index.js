de337e06b65562f3c34c1802e63c7fac3917b1a3
Annie Eby <eby.annie@gmail.com>
2018-08-21 16:51:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a,b) {console.log(${a}+${b})}
+add(1,80)
+
+function subtract(a,b) {console.log(${a}-${b})}
+subtract(60,40)
+
+function multiply(a,b) {console.log(${a}*${b})}
+multiply(2,3.4)
+
+function divide(a,b) {console.log(${a}/${b})}
+divide(5.0,2.5)
+
+function inc(n) {console.log(number+${n})}
+
+function dec(n) {console.log(number-${n})}
+
+function makeInt(n) {console.log(parseInt(n))
+
+var number = 5
+
+
+
+
+
