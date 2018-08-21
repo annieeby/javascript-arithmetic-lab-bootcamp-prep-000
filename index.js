04cf8115ac667d17bf031c0a36a75973f15172a7
Annie Eby <eby.annie@gmail.com>
2018-08-21 17:08:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ function dec(n) {return(n-1)}
 
 function makeInt(n) {return parseInt(n,10)}
 
+function preserveDecimal(n) {return parseFloat(n,10)}
 
+function preserveDecimal(n) {return parseFloat(n,10)}
 
 /*
 function add(a,b) {console.log(a+b)}
