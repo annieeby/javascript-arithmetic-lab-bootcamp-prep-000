3a2bea2c14a70572b53150c26e1650a2285c8cf0
Annie Eby <eby.annie@gmail.com>
2018-08-21 16:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ function subtract(a,b) {console.log(${a}-${b})}
 
 
 function multiply(a,b) {console.log(${a}*${b})}
-multiply(2,3.4)
+
 
 function divide(a,b) {console.log(${a}/${b})}
-divide(5.0,2.5)
+
 
 function inc(n) {console.log(number+${n})}
 
+
 function dec(n) {console.log(number-${n})}
 
+
 function makeInt(n) {console.log(parseInt(n))
+
 
 var number = 5
 
